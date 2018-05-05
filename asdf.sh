5e7410cfc6b6524ba3eca2c57011f4c9487b75ef
Maddison Hellstrom <maddy@na.ai>
2018-05-05 09:35:20 -0700

    Call cd in bash subprocess to avoid side-effects

--- asdf.sh
+++ asdf.sh
@@ -7,7 +7,7 @@ else
 fi
 
 export ASDF_DIR
-ASDF_DIR="$(cd "$(dirname "$current_script_path")" &> /dev/null || exit 1; pwd)"
+ASDF_DIR="$(cdpath="$(dirname "$current_script_path")" bash -c 'cd "$cdpath" &> /dev/null || exit 1; pwd')"
 
 # Add asdf to PATH
 #
